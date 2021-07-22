e8b8ab8b067ff501edc259bdc1ab10d94146fb1b
Roman Tkalenko <rmn.tk.ml@gmail.com>
2021-07-22 19:34:28 +0300

    more tests

--- src/test/kotlin/com/tkroman/kerl/parser/TypeAwareCallBodyParserTest.kt
+++ src/test/kotlin/com/tkroman/kerl/parser/TypeAwareCallBodyParserTest.kt
@@ -9,6 +9,7 @@ import com.tkroman.kerl.model.Unknown
 import com.tkroman.kerl.model.ValidRpcCall
 import io.appulse.encon.terms.Erlang.NIL
 import io.appulse.encon.terms.Erlang.atom
+import io.appulse.encon.terms.Erlang.bstring
 import io.appulse.encon.terms.Erlang.list
 import io.appulse.encon.terms.Erlang.tuple
 import io.appulse.encon.terms.type.ErlangAtom.ATOM_TRUE
@@ -116,6 +117,72 @@ internal class TypeAwareCallBodyParserTest {
         )
         assertEquals(
             InvalidRpcCall(GEN_CALL_TYPE, "no module"),
+            parser.parse(call, GEN_CALL_TYPE)
+        )
+    }
+
+    @Test
+    fun `module is not an atom - invalid rpc call`() {
+        val call = tuple(
+            CALL,
+            bstring("not-an-atom")
+        )
+        assertEquals(
+            InvalidRpcCall(REX_CALL_TYPE, "no module"),
+            parser.parse(call, REX_CALL_TYPE)
+        )
+        assertEquals(
+            InvalidRpcCall(GEN_CALL_TYPE, "no module"),
+            parser.parse(call, GEN_CALL_TYPE)
+        )
+    }
+
+    @Test
+    fun `module is empty - invalid rpc call`() {
+        val call = tuple(
+            CALL,
+            atom("")
+        )
+        assertEquals(
+            InvalidRpcCall(REX_CALL_TYPE, "no module"),
+            parser.parse(call, REX_CALL_TYPE)
+        )
+        assertEquals(
+            InvalidRpcCall(GEN_CALL_TYPE, "no module"),
+            parser.parse(call, GEN_CALL_TYPE)
+        )
+    }
+
+    @Test
+    fun `function is not an atom - invalid rpc call`() {
+        val call = tuple(
+            CALL,
+            atom("foo"),
+            bstring("bar")
+        )
+        assertEquals(
+            InvalidRpcCall(REX_CALL_TYPE, "no function"),
+            parser.parse(call, REX_CALL_TYPE)
+        )
+        assertEquals(
+            InvalidRpcCall(GEN_CALL_TYPE, "no function"),
+            parser.parse(call, GEN_CALL_TYPE)
+        )
+    }
+
+    @Test
+    fun `function is empty - invalid rpc call`() {
+        val call = tuple(
+            CALL,
+            atom("foo"),
+            bstring("")
+        )
+        assertEquals(
+            InvalidRpcCall(REX_CALL_TYPE, "no function"),
+            parser.parse(call, REX_CALL_TYPE)
+        )
+        assertEquals(
+            InvalidRpcCall(GEN_CALL_TYPE, "no function"),
             parser.parse(call, GEN_CALL_TYPE)
         )
     }
